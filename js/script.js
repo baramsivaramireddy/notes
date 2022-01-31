14970ef3034f3013d60cb0df9180f30837acec84
baramsivaramireddy <baramsivarami@gmail.com>
2022-02-01 00:17:42 +0530

    added responsive menu and google analytics code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 //$("button").has("a").css("border-color","#0f0");
+
+
+var button = document.getElementById('menu'); // Assumes element with id='button'
+
+button.onclick = function() {
+    var div = document.getElementById('phone_menu');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'flex';
+        div.style.flexDirection= 'column'
+        div.style.textAlign = "center"
+        div.style.textDecoration = "none"
+        div.style.color = "white"
+        div.style.marginTop= "1rem"
+        
+       
+
+    }
+};
